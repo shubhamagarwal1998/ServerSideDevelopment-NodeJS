3f01dfd06545e3988fa83d724df91ca68b63ec67
shubhamagarwal1998 <agarwalpshubham1998@gmail.com>
2020-05-14 23:57:46 +0530

    Added Introduction to Express and Express Router

---
+++ node-express/index.js
@@ -0,0 +1,43 @@
+const express=require('express'),http=require('http');
+
+const hostname="localhost";
+const port=3000;
+const morgan=require('morgan');
+const bodyParser=require('body-parser');
+const dishRouter=require('./routes/dishRouter');
+
+const app=express();
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use('/dishes',dishRouter);
+
+app.get('/dishes/:dishId',(req,res,next) =>{
+    res.end('Will send details of the dish: ' + req.params.dishId +' to you!');
+});
+
+app.put('/dishes/:dishId',(req,res,next) =>{
+    res.write('Updating the dish: ' + req.params.dishId + '\n');
+    res.end('Will update the dish: ' + req.body.name + 
+        ' with details: ' + req.body.description);
+});
+
+app.post('/dishes/:dishId',(req,res,next) =>{
+    res.statusCode = 403;
+    res.end('POST operation not supported on /dishes/'+ req.params.dishId);
+});
+
+app.delete('/dishes/:dishId',(req,res,next) =>{
+    res.end('Deleting dish: ' + req.params.dishId);
+});
+app.use(express.static(__dirname +'/public'));
+
+app.use((req,res,next)=> {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<html><body><h1>This is an Express Server</h1></body></html>');
+});
+const server=http.createServer(app);
+
+server.listen(port,hostname,() =>{
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
