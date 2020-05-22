5afc67514e033af8344f81018a5c34a884bb4cb2
shubhamagarwal1998 <agarwalpshubham1998@gmail.com>
2020-05-22 20:11:14 +0530

    Added REST API and Mongoose 2 in previous commit and added assignment 2 in this commit

--- conFusionServer/routes/dishRouter.js
+++ conFusionServer/routes/dishRouter.js
@@ -43,39 +43,39 @@ dishRouter.route('/')
 });
 
 dishRouter.route('/:dishId')
-    .get((req, res, next) => {
-        Dishes.findById(req.params.dishId)
-        .then((dish)=>{
-            res.statusCode=200;
-            res.setHeader('Content-Type','application/json');
-            res.json(dish);
-        },(err)=>next(err))
-        .catch((err)=>next(err));
-    })
-    .post((req, res, next) => {
-        res.statusCode = 403;
-        res.end('POST operation is not permitted on this route /dishes/' + req.params.dishId);
-    })
-    .put((req, res, next) => {
-        Dishes.findByIdAndUpdate(req.params.dishId,{
-            $set:req.body
-        },{new:true})
-        .then((dish)=>{
-            res.statusCode=200;
-            res.setHeader('Content-Type','application/json');
-            res.json(dish);
-        },(err)=>next(err))
-        .catch((err)=>next(err));       
-    })
-    .delete((req, res, next) => {
-        Dishes.findByIdAndRemove(req.params.dishId)
-            .then((resp) => {
-                res.statusCode = 200;
-                res.setHeader('Content-Type', 'application/json');
-                res.json(resp);
-            }, (err) => next(err))
-            .catch((err) => next(err));
-    });
+.get((req, res, next) => {
+    Dishes.findById(req.params.dishId)
+    .then((dish)=>{
+        res.statusCode=200;
+        res.setHeader('Content-Type','application/json');
+        res.json(dish);
+    },(err)=>next(err))
+    .catch((err)=>next(err));
+})
+.post((req, res, next) => {
+    res.statusCode = 403;
+    res.end('POST operation is not permitted on this route /dishes/' + req.params.dishId);
+})
+.put((req, res, next) => {
+    Dishes.findByIdAndUpdate(req.params.dishId,{
+        $set:req.body
+    },{new:true})
+    .then((dish)=>{
+        res.statusCode=200;
+        res.setHeader('Content-Type','application/json');
+        res.json(dish);
+    },(err)=>next(err))
+    .catch((err)=>next(err));       
+})
+.delete((req, res, next) => {
+    Dishes.findByIdAndRemove(req.params.dishId)
+        .then((resp) => {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json(resp);
+        }, (err) => next(err))
+        .catch((err) => next(err));
+});
 
 dishRouter.route('/:dishId/comments')
 .get((req,res,next) =>{
--- conFusionServer/routes/leaderRouter.js
+++ conFusionServer/routes/leaderRouter.js
@@ -1,50 +1,79 @@
 const express=require('express');
 const bodyParser=require('body-parser');
 
+const Leaders=require('../models/leaders');
+
 const leaderRouter=express.Router();
 
 leaderRouter.use(bodyParser.json());
 
 leaderRouter.route('/')
-.all((req,res,next)=>{
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    next();   
-})
-
 .get((req,res,next) =>{
-    res.end("Will send all the leader");
+    Leaders.find({})
+    .then((leaders)=>{
+        res.statusCode=200;
+        res.setHeader('Content-Type','application/json');
+        res.json(leaders);
+    },(err)=>next(err))
+    .catch((err)=>next(err));
 })
 
 .put((req,res,next) =>{
     res.statusCode = 403;
-  res.end('PUT operation not supported on /leader');
+  res.end('PUT operation not supported on /leaders');
 })
 .post((req,res,next) =>{
-    res.end('Will add the leader: ' + req.body.name + ' with details: ' + req.body.description);
+    Leaders.create({})
+    .then((leader)=>{
+        res.statusCode=200;
+        res.setHeader('Content-Type','application/json');
+        res.json(leader);
+    },(err)=>next(err))
+    .catch((err)=>next(err));
 })
 .delete((req,res,next) =>{
-    res.end("Deleting all the leader");
+    Leaders.remove({})
+    .then((resp)=>{
+        res.statusCode=200;
+        res.setHeader('Content-Type','application/json');
+        res.json(resp);
+    },(err)=>next(err))
+    .catch((err)=>next(err));
 });
 
 leaderRouter.route('/:leaderId')
-.get((req,res,next) =>{
-    res.end('Will send details of the leader: ' + req.params.leaderId +' to you!');
+.get((req, res, next) => {
+    Leaders.findById(req.params.leaderId)
+    .then((leader)=>{
+        res.statusCode=200;
+        res.setHeader('Content-Type','application/json');
+        res.json(leader);
+    },(err)=>next(err))
+    .catch((err)=>next(err));
 })
-
-.put((req,res,next) =>{
-    res.write('Updating the leader: ' + req.params.leaderId + '\n');
-    res.end('Will update the leader: ' + req.body.name + 
-        ' with details: ' + req.body.description);
+.post((req, res, next) => {
+    res.statusCode = 403;
+    res.end('POST operation is not permitted on this route /Leaders/' + req.params.leaderId);
 })
-
-.post((req,res,next) =>{
-    res.statusCode = 403;                                                                                           
-    res.end('POST operation not supported on /leader/'+ req.params.leaderId);
+.put((req, res, next) => {
+    Leaders.findByIdAndUpdate(req.params.leaderId,{
+        $set:req.body
+    },{new:true})
+    .then((leader)=>{
+        res.statusCode=200;
+        res.setHeader('Content-Type','application/json');
+        res.json(leader);
+    },(err)=>next(err))
+    .catch((err)=>next(err));       
 })
-
-.delete((req,res,next) =>{
-    res.end('Deleting leader: ' + req.params.leaderId);
+.delete((req, res, next) => {
+    Leaders.findByIdAndRemove(req.params.leaderId)
+        .then((resp) => {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json(resp);
+        }, (err) => next(err))
+        .catch((err) => next(err));
 });
 
 
